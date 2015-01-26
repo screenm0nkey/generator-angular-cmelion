5bfe9cad9ef932c2446650aaff170834f3c24e0b
Nick Lowman <digital@nicklowman.co.uk>
2015-01-26 09:15:27 +0000

    Added component module name, based on the location of the module in a nested folder structure. Did a little code tidy. Note, the module currently have to be manually added to the scripts/module.js after creating them. I'm will eventually add a grunt task which reads all the component module names and adds them to the scripts/module.js

--- component/index.js
+++ component/index.js
@@ -8,36 +8,43 @@ var _ = require('underscore.string');
 
 var Generator = module.exports = function Generator() {
   ScriptBase.apply(this, arguments);
-
   yeoman.generators.NamedBase.apply(this, arguments);
+
   this.sourceRoot(path.join(__dirname, '../templates/javascript/'));
 
   if (typeof this.env.options.appPath === 'undefined') {
     try {
-        this.env.options.appPath = require(path.join(process.cwd(), 'bower.json')).appPath;
-    } catch (e) {}
+      this.env.options.appPath = require(path.join(process.cwd(), 'bower.json')).appPath;
+    } catch (e) {
+    }
     this.env.options.appPath = this.env.options.appPath || 'src';
   }
-
 };
 
 util.inherits(Generator, ScriptBase);
 
 Generator.prototype.createComponentFiles = function createComponentFiles() {
-    this.viewClassesForScss = '.' + this.dasherizedName + '-component';
-    this.viewClassesForHTML = this.dasherizedName + '-component';
-    var targetPath = this.slugifiedPath.join('/') + '/' + this.dasherizedName;
+  var targetPath = this.slugifiedPath.join('/') + '/' + this.dasherizedName;
 
-    this.generateSourceAndTest(
-        'component',
-        'spec/component',
-        ('../components/' + _.dasherize(this.name)),
-        '../unit/spec/components/' + targetPath
-    );
+  this.componentCtrlName = this.cameledName + 'ComponentCtrl';
+  this.componentModuleName = this.scriptAppName + '.components.' + this.name.replace(/\//g, '.');
+  this.templateUrl = ('components/' + targetPath + '/' + this.dasherizedName + '.html').replace(/\/\//g, '/');
+  this.viewClassesForScss = '.' + this.dasherizedName + '-component';
+  this.viewClassesForHTML = this.dasherizedName + '-component';
 
+  var newSassTemplatePath = path.join(this.env.options.appPath, 'components', targetPath, '_' + this.dasherizedName + '.scss');
+  var newHtmlTemplatePath = path.join(this.env.options.appPath, 'components', targetPath, this.dasherizedName + '.html');
 
-    this.template('../common/component.scss', path.join(this.env.options.appPath, 'components', targetPath, '_' + this.dasherizedName + '.scss'));
-    this.addStyleToComponentScss('../components/' + targetPath + '/' + this.dasherizedName);
+  this.generateSourceAndTest(
+    'component',
+    'spec/component',
+    ('../components/' + _.dasherize(this.name)),
+    '../unit/spec/components/' + targetPath
+  );
 
-    this.template('../common/component.html', path.join(this.env.options.appPath, 'components', targetPath, this.dasherizedName + '.html'));
+  this.template('../common/component.scss', newSassTemplatePath);
+  this.addStyleToComponentScss('../components/' + targetPath + '/' + this.dasherizedName);
+
+  this.template('../common/component.html', newHtmlTemplatePath);
+  debugger
 };
--- templates/javascript/component.js
+++ templates/javascript/component.js
@@ -1,21 +1,20 @@
 (function(){
   'use strict';
 
-  angular.module('<%= scriptAppName %>.components')
-      .controller('<%= cameledName %>ComponentCtrl', controllerFunction)
-      .component('<%= cameledName %>', componentFunction);
+  angular.module('<%= componentModuleName %>')
+    .controller('<%= componentCtrlName %>', controllerFunction)
+    .component('<%= cameledName %>', componentFunction);
 
-  function controllerFunction ($scope, $element) {
+  function controllerFunction ($scope, $element, $attrs) {
     var vm = this;
-    vm.text = 'this is the <%= name %> component';
+    vm.text = 'this is the <%= cameledName %> component';
   }
 
   function componentFunction () {
     return {
-      controller: '<%= cameledName %>ComponentCtrl',
-      controllerAs: 'vm'
+      templateUrl: '<%= templateUrl %>',
+      controller: '<%= componentCtrlName %>'
     };
   }
-
 }());
 
